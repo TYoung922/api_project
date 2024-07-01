3f2d09e972ae4a9f9073b8fdc734ec01edd38d84
TercanoNuruva <thyoung89@gmail.com>
2024-07-01 14:10:01 -0600

    add update and delete

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const routes = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
 
-routes.get("/ttech", myController.tooeleTech);
+routes.get("/ttech", myController.getAllStudents);
 
-routes.get("/students", myController.getAllStudents);
+// routes.get("/students", myController.getAllStudents);
+
+// student routes
+routes.use("/students", require("./students"));
 
 module.exports = routes;
